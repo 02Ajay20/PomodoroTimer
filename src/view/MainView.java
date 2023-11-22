1f1029a7a0e0f2de6c44bb9928c8fdec45a5ca0a
unknown <oscarandreshernandezpineda@gmail.com>
2023-11-21 23:39:55 -0500

    GUI modified (plz help this mf gui is hard asf to make it look good, just f kill me dude cmon)

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -1,6 +1,9 @@
 package view;
 
+import java.awt.Color;
 import java.awt.Font;
+
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -8,11 +11,12 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
 
 import controller.MainControl;
 
 public class MainView extends JFrame{
-    public JButton closeButton;
+    public JButton closeButton, startButton, defaultButton, aboutButton;
     public JLabel title, workLabel, breakLabel;
     public JPanel panel;
     public ImageIcon close;
@@ -30,6 +34,7 @@ public class MainView extends JFrame{
     public void GUI(){
 
         panel = new JPanel();
+        panel.setBackground(Color.decode("#09C4FB"));
 
         //----------- Close Button ---------------------------
 
@@ -46,38 +51,74 @@ public class MainView extends JFrame{
         title = new JLabel("Pomodoro Timer");
         title.setBounds(280, 20, 300, 80);
         title.setFont(new Font("Tahoma", Font.BOLD, 32));
+        title.setForeground(Color.decode("#09374e"));
         panel.add(title);
 
         //----------- Work Time section ---------------------------
 
         workLabel = new JLabel("Work Time");
-        workLabel.setBounds(90, 150, 300, 80);
+        workLabel.setBounds(240, 150, 300, 80);
         workLabel.setFont(new Font("Tahoma", Font.BOLD, 22));
+        workLabel.setForeground(Color.decode("#09374e"));
         panel.add(workLabel);
       
         workTextField = new JTextField();
-        workTextField.setBounds(250, 180, 100, 30);
+        workTextField.setBounds(400, 180, 100, 30);
+        workTextField.setEditable(false);
         panel.add(workTextField);
              
         workComboBox = new JComboBox<>(new String[]{"10", "20", "30", "Personalizar"});
-        workComboBox.setBounds(360, 180, 80, 30);
+        workComboBox.setBounds(510, 180, 80, 30);
         panel.add(workComboBox);
 
         //----------- Break Time section ---------------------------
 
         breakLabel = new JLabel("Break Time");
-        breakLabel.setBounds(90, 250, 300, 80);
+        breakLabel.setBounds(240, 220, 300, 80);
         breakLabel.setFont(new Font("Tahoma", Font.BOLD, 22));
+        breakLabel.setForeground(Color.decode("#09374e"));
         panel.add(breakLabel);
 
         breakTextField = new JTextField();
-        breakTextField.setBounds(250, 280, 100, 30);
+        breakTextField.setBounds(400, 250, 100, 30);
+        breakTextField.setEditable(false);
         panel.add(breakTextField);
 
         breakComboBox = new JComboBox<>(new String[]{"5", "10", "15", "Personalizar"});
-        breakComboBox.setBounds(360, 280, 80, 30);
+        breakComboBox.setBounds(510, 250, 80, 30);
         panel.add(breakComboBox);
 
+        //----------- Buttons section ------------------------------
+
+        //-About button---------------------------------------------
+        aboutButton = new JButton("About");
+        aboutButton.setBounds(340, 420, 140, 40);
+        aboutButton.setFont(new Font("Arial", Font.BOLD, 20));
+        aboutButton.setBackground(Color.decode("#8a8a8a"));
+        aboutButton.setForeground(Color.decode("#ededed"));
+        Border aboutButtonbBorder = BorderFactory.createLineBorder(Color.decode("#ccd4d9"));
+        aboutButton.setBorder(aboutButtonbBorder);
+        panel.add(aboutButton);
+
+        //-Start Button---------------------------------------------
+        startButton = new JButton("Start");
+        startButton.setBounds(260, 350, 140, 40);
+        startButton.setFont(new Font("Arial", Font.BOLD, 20));
+        startButton.setBackground(Color.decode("#02ba05"));
+        startButton.setForeground(Color.decode("#ededed"));
+        Border startButtonBorder = BorderFactory.createLineBorder(Color.decode("#ededed"));
+        startButton.setBorder(startButtonBorder);
+        panel.add(startButton);
+
+        //-Default Button---------------------------------------------
+        defaultButton = new JButton("Default");
+        defaultButton.setBounds(430, 350, 140, 40);
+        defaultButton.setFont(new Font("Arial", Font.BOLD, 20));
+        defaultButton.setBackground(Color.decode("#ba2d02"));
+        defaultButton.setForeground(Color.decode("#ededed"));
+        Border defaultButtonBorder = BorderFactory.createLineBorder(Color.decode("#ededed"));
+        defaultButton.setBorder(defaultButtonBorder);
+        panel.add(defaultButton);
 
 
         add(panel);
