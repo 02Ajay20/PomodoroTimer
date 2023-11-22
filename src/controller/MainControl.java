d11056142bcdab9fce73aa80eea2509b5b1d0a3c
unknown <oscarandreshernandezpineda@gmail.com>
2023-11-22 14:02:05 -0500

    controller on buttons added

--- src/controller/MainControl.java
+++ src/controller/MainControl.java
@@ -2,6 +2,8 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 import view.MainView;
 
@@ -15,6 +17,36 @@ public class MainControl {
             @Override
             public void actionPerformed(ActionEvent e){
                 objMainView.dispose();
+            }
+        });
+
+        objMainView.workComboBox.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    String item = (String) e.getItem();
+                    if (item.equals("Personalizar")) {
+                        objMainView.workTextField.setEnabled(true);
+                    } else {
+                        objMainView.workTextField.setEnabled(false);
+                        objMainView.workTextField.setText(item);
+                    }
+                }
+            }
+        });
+
+        objMainView.breakComboBox.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    String item = (String) e.getItem();
+                    if (item.equals("Personalizar")) {
+                        objMainView.breakTextField.setEnabled(true);
+                    } else {
+                        objMainView.breakTextField.setEnabled(false);
+                        objMainView.breakTextField.setText(item);
+                    }
+                }
             }
         });
     }
--- src/view/MainView.java
+++ src/view/MainView.java
@@ -3,14 +3,7 @@ package view;
 import java.awt.Color;
 import java.awt.Font;
 
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.*;
 import javax.swing.border.Border;
 
 import controller.MainControl;
@@ -24,7 +17,7 @@ public class MainView extends JFrame{
     public JComboBox<String> workComboBox, breakComboBox;
 
     public MainView(){
-        setSize(800, 600);
+        setSize(600, 450);
         setUndecorated(true);
         setLocationRelativeTo(null);
         setVisible(true);
@@ -40,7 +33,7 @@ public class MainView extends JFrame{
 
         close = new ImageIcon(getClass().getResource("../images/close.png"));
         closeButton = new JButton(close);
-        closeButton.setBounds(750, 5, 45, 45);
+        closeButton.setBounds(550, 5, 45, 45);
         closeButton.setContentAreaFilled(false);
         closeButton.setBorderPainted(false);
         panel.add(closeButton);
@@ -49,50 +42,50 @@ public class MainView extends JFrame{
         //----------- Title ---------------------------
 
         title = new JLabel("Pomodoro Timer");
-        title.setBounds(280, 20, 300, 80);
-        title.setFont(new Font("Tahoma", Font.BOLD, 32));
+        title.setBounds(160, 20, 300, 80);
+        title.setFont(new Font("Tahoma", Font.BOLD, 34));
         title.setForeground(Color.decode("#09374e"));
         panel.add(title);
 
         //----------- Work Time section ---------------------------
 
         workLabel = new JLabel("Work Time");
-        workLabel.setBounds(240, 150, 300, 80);
+        workLabel.setBounds(140, 140, 300, 80);
         workLabel.setFont(new Font("Tahoma", Font.BOLD, 22));
         workLabel.setForeground(Color.decode("#09374e"));
         panel.add(workLabel);
       
         workTextField = new JTextField();
-        workTextField.setBounds(400, 180, 100, 30);
-        workTextField.setEditable(false);
+        workTextField.setBounds(300, 170, 100, 30);
+        workTextField.setEnabled(false);
         panel.add(workTextField);
              
         workComboBox = new JComboBox<>(new String[]{"10", "20", "30", "Personalizar"});
-        workComboBox.setBounds(510, 180, 80, 30);
+        workComboBox.setBounds(410, 170, 80, 30);
         panel.add(workComboBox);
 
         //----------- Break Time section ---------------------------
 
         breakLabel = new JLabel("Break Time");
-        breakLabel.setBounds(240, 220, 300, 80);
+        breakLabel.setBounds(140, 200, 300, 80);
         breakLabel.setFont(new Font("Tahoma", Font.BOLD, 22));
         breakLabel.setForeground(Color.decode("#09374e"));
         panel.add(breakLabel);
 
         breakTextField = new JTextField();
-        breakTextField.setBounds(400, 250, 100, 30);
-        breakTextField.setEditable(false);
+        breakTextField.setBounds(300, 230, 100, 30);
+        breakTextField.setEnabled(false);
         panel.add(breakTextField);
 
         breakComboBox = new JComboBox<>(new String[]{"5", "10", "15", "Personalizar"});
-        breakComboBox.setBounds(510, 250, 80, 30);
+        breakComboBox.setBounds(410, 230, 80, 30);
         panel.add(breakComboBox);
 
         //----------- Buttons section ------------------------------
 
         //-About button---------------------------------------------
         aboutButton = new JButton("About");
-        aboutButton.setBounds(340, 420, 140, 40);
+        aboutButton.setBounds(240, 400, 140, 40);
         aboutButton.setFont(new Font("Arial", Font.BOLD, 20));
         aboutButton.setBackground(Color.decode("#8a8a8a"));
         aboutButton.setForeground(Color.decode("#ededed"));
@@ -102,7 +95,7 @@ public class MainView extends JFrame{
 
         //-Start Button---------------------------------------------
         startButton = new JButton("Start");
-        startButton.setBounds(260, 350, 140, 40);
+        startButton.setBounds(160, 320, 140, 40);
         startButton.setFont(new Font("Arial", Font.BOLD, 20));
         startButton.setBackground(Color.decode("#02ba05"));
         startButton.setForeground(Color.decode("#ededed"));
@@ -112,9 +105,9 @@ public class MainView extends JFrame{
 
         //-Default Button---------------------------------------------
         defaultButton = new JButton("Default");
-        defaultButton.setBounds(430, 350, 140, 40);
+        defaultButton.setBounds(330, 320, 140, 40);
         defaultButton.setFont(new Font("Arial", Font.BOLD, 20));
-        defaultButton.setBackground(Color.decode("#ba2d02"));
+        defaultButton.setBackground(Color.decode("#0298ba"));
         defaultButton.setForeground(Color.decode("#ededed"));
         Border defaultButtonBorder = BorderFactory.createLineBorder(Color.decode("#ededed"));
         defaultButton.setBorder(defaultButtonBorder);
